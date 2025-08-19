e6eabfa98cd53ec95f9d940c9d124c6a00bbbad1
Roshan <roshansubedi77.com>
2025-08-19 10:23:52 +0545

    chore(deps): installed fastapi

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+
+@app.get("/")
 def main():
-    print("Hello from fight-nepal!")
+ return("Hello from fight-nepal!")
 
 
 if __name__ == "__main__":
